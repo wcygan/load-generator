4bace46869f23ba2129c8fe76b3182de06e7bc6e
Will Cygan <wcygan.io@gmail.com>
2023-03-09 18:34:13 -0800

    move out of lib-wc and into the load-generator repostiory

---
+++ load-generator-application/src/main.rs
@@ -0,0 +1,54 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+use load_generator_common::run;
+use std::time::Duration;
+use tokio::select;
+use tokio::time::Instant;
+use tracing::debug;
+
+mod args;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = args::Cli::parse();
+    match tracing_subscriber::fmt::try_init() {
+        Ok(_) => {}
+        Err(_) => {
+            tracing_subscriber::fmt().init();
+        }
+    }
+
+    run(cli.url, cli.connections, shutdown(cli.time))
+        .await
+        .context("error running load generator")
+        .unwrap();
+
+    Ok(())
+}
+
+/// Creates a future that will be used to signal shutdown.
+///
+/// If a time is provided, the future will resolve after that time.
+/// Otherwise, it will resolve when the user presses Ctrl+C.
+async fn shutdown(time: Option<u16>) {
+    match time {
+        None => {
+            debug!("Press Ctrl+C to stop");
+            select! {
+                _ = tokio::signal::ctrl_c() => {}
+            }
+        }
+        Some(t) => {
+            debug!("Running for {} seconds", t);
+            debug!("Press Ctrl+C to stop early");
+            let mut interval = tokio::time::interval_at(
+                Instant::now() + Duration::from_secs(t.into()),
+                Duration::from_secs(1),
+            );
+            select! {
+                _ = interval.tick() => {}
+                _ = tokio::signal::ctrl_c() => {}
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,48 @@
+## Load Generator
+
+---
+
+A tool to send load to a server.
+
+This is accomplished by sending as many requests as possible to a server in a given time period.
+
+#### Warning
+
+> You probably don't want to target a host on the internet or use a metered connection with this :)
+
+### Installation
+
+```bash
+cargo install --git https://github.com/wcygan/load-generator
+```
+
+### Usage
+
+```
+A tool to load test a server
+
+Usage: load-generator-application [OPTIONS] --url <URL>
+
+Options:
+  -u, --url <URL>                  The URL to send requests to
+  -c, --connections <CONNECTIONS>  The number of connections to use [default: 8]
+  -t, --time <TIME>                The amount of seconds to run the test for. If not specified, the test will run until an interrupt signal is received
+  -h, --help                       Print help
+
+```
+
+### Example
+
+In this example we'll target a local server running on port 3000 with 2 connections for 5 seconds.
+
+Running [Axum Hello Server](https://github.com/wcygan/lib-wc/tree/master/experiments/axum-hello-server) locally & aiming this tool at it yields around 1M - 2M QPS.
+
+```
+ldg --url 0.0.0.0:3000 --connections 4 --time 2
+```
+
+On a MacBook Pro M1 Max this is running at approximately 1898470.07 requests per second.
+
+### References
+
+Adapted from [A Load Test CLI with Async Rust](https://www.manning.com/liveprojectseries/a-load-test-cli-with-async-rust-ser)
